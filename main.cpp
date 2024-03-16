9ac192caf297169e900a68c27c71664567ebc368
BachiBachBach <thehessboi@gmail.com>
2024-03-16 18:41:26 -0500

    new commit

--- main.cpp
+++ main.cpp
@@ -8,57 +8,59 @@ void printBoard(string board[3][3]);
 
 void playTurn(string board[3][3], string player);
 
+bool validateInput(string board[3][3], int rowSelection, int colSelection);
+
+string checkForWin(string board[3][3]);
+
 // constants
 
 const int ROWS = 3;
 const int COLS = 3;
 
-
-
-
-
 const string PLAYER_1 = "X";
 const string PLAYER_2 = "O";
-
-
 
 int main()
 {
     string board[ROWS][COLS] = {
-    {" * ", " * ", " * "},
-    {" * ", " * ", " * "},
-    {" * ", " * ", " * "},
-};
+        {" * ", " * ", " * "},
+        {" * ", " * ", " * "},
+        {" * ", " * ", " * "},
+    };
     bool running = true;
-    
+
     int turn = 0;
-    
+
     while (running)
     {
-    printBoard(board);
+        printBoard(board);
 
+        if (turn == 0)
+        {
+            playTurn(board, PLAYER_1);
+            turn = 1;
+        }
+        else
+        {
+            playTurn(board, PLAYER_2);
+            turn = 0;
+        }
 
+        cout << checkForWin(board) << endl;
+    }
+}
 
-    if (turn == 0)
+bool validateInput(string board[3][3], int rowSelection, int colSelection)
+{
+    if (board[rowSelection - 1][colSelection - 1] != " X " && board[rowSelection - 1][colSelection - 1] != " O ")
     {
-        playTurn(board, PLAYER_1);
-        turn = 1;
+        return true;
     }
     else
     {
-        playTurn(board, PLAYER_2);
-        turn = 0;
+        return false;
     }
-
-
-
-
-
-    }
-
-
 }
-
 
 void printBoard(string board[3][3])
 {
@@ -74,23 +76,30 @@ void printBoard(string board[3][3])
 
 void playTurn(string board[3][3], string player)
 {
-    
+
     int rowSelection;
     int colSelection;
-    if (rowSelection < 4 && rowSelection > 0 && colSelection < 4 && colSelection > 0)
-    {
-    cout << "Player " <<  player << " turn" << endl;
+
+    cout << "Player " << player << " turn" << endl;
     cout << "Enter a row(1-3): ";
     cin >> rowSelection;
     cout << "Enter a col(1-3): ";
     cin >> colSelection;
 
-    
-
-    board[rowSelection - 1][colSelection - 1] = " " + player + " ";
+    if (rowSelection < 4 && rowSelection > 0 && colSelection < 4 && colSelection > 0 && validateInput(board, rowSelection, colSelection) == true)
+    {
+        // cout << board[rowSelection - 1][colSelection - 1];
+        board[rowSelection - 1][colSelection - 1] = " " + player + " ";
     }
     else
     {
+        cout << "Invalid Input. Try again" << endl;
         playTurn(board, player);
     }
+}
+
+string checkForWin(string board[3][3])
+{
+
+    return "";
 }
