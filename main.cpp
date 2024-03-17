f0ac6d2773956830904094887a9ce76723646a50
BachiBachBach <thehessboi@gmail.com>
2024-03-17 16:13:39 -0500

    final commit

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ bool validateInput(string board[3][3], int rowSelection, int colSelection);
 
 string checkForWin(string board[3][3]);
 
+bool checkForDraw(string board[3][3]);
+
 // constants
 
 const int ROWS = 3;
@@ -30,23 +32,37 @@ int main()
     bool running = true;
 
     int turn = 0;
-
+    int times = 0;
     while (running)
     {
+
         printBoard(board);
 
         if (turn == 0)
         {
             playTurn(board, PLAYER_1);
+            times++;
             turn = 1;
         }
         else
         {
+            times++;
             playTurn(board, PLAYER_2);
             turn = 0;
         }
 
-        cout << checkForWin(board) << endl;
+        if (checkForWin(board) == "X" || checkForWin(board) == "O")
+        {
+            printBoard(board);
+            cout << "Player " << checkForWin(board) << " won the game!" << endl;
+            break;
+        }
+
+        if (times == 9 && checkForDraw(board) == true)
+        {
+            cout << "Cat won!" << endl;
+            break;
+        }
     }
 }
 
@@ -100,6 +116,61 @@ void playTurn(string board[3][3], string player)
 
 string checkForWin(string board[3][3])
 {
+    // CHECKING FOR WIN IN EACH ROW
+    for (int i = 0; i < ROWS; i++)
+    {
+        if (board[i][0] == " " + PLAYER_1 + " " && board[i][1] == " " + PLAYER_1 + " " && board[i][2] == " " + PLAYER_1 + " ")
+        {
+            return PLAYER_1;
+        }
+        else if (board[i][0] == " " + PLAYER_2 + " " && board[i][1] == " " + PLAYER_2 + " " && board[i][2] == " " + PLAYER_2 + " ")
+        {
+            return PLAYER_2;
+        }
+    }
 
-    return "";
+    // CHECKING FOR WIN IN EACH COLUMN
+    for (int j = 0; j < COLS; j++)
+    {
+        if (board[0][j] == " " + PLAYER_1 + " " && board[1][j] == " " + PLAYER_1 + " " && board[2][j] == " " + PLAYER_1 + " ")
+        {
+            return PLAYER_1;
+        }
+        else if (board[0][j] == " " + PLAYER_2 + " " && board[1][j] == " " + PLAYER_2 + " " && board[2][j] == " " + PLAYER_2 + " ")
+        {
+            return PLAYER_2;
+        }
+    }
+
+    if (board[0][0] == " " + PLAYER_1 + " " && board[1][1] == " " + PLAYER_1 + " " && board[2][2] == " " + PLAYER_1 + " " ||
+        board[0][2] == " " + PLAYER_1 + " " && board[1][1] == " " + PLAYER_1 + " " && board[2][0] == " " + PLAYER_1 + " ")
+    {
+        return PLAYER_1;
+    }
+    else if (board[0][0] == " " + PLAYER_2 + " " && board[1][1] == " " + PLAYER_2 + " " && board[2][2] == " " + PLAYER_2 + " " ||
+             board[0][2] == " " + PLAYER_2 + " " && board[1][1] == " " + PLAYER_2 + " " && board[2][0] == " " + PLAYER_2 + " ")
+    {
+        return PLAYER_2;
+    }
+    return "_";
+}
+
+bool checkForDraw(string board[3][3])
+{
+
+    for (int i = 0; i < ROWS; i++)
+    {
+        for (int j = 0; j < COLS; j++)
+        {
+            if (board[i][j] == " " + PLAYER_1 + " " || board[i][j] == " " + PLAYER_2 + " ")
+            {
+            }
+            else
+            {
+                return false;
+                break;
+            }
+            return true;
+        }
+    }
 }
